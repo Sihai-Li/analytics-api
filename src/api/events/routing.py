a11d6e029cc2e82e8dbe8cf18e2bc0b173cf50f6
Sihai Li <shli2020@outlook.com>
2025-07-25 00:45:52 -0500

    Add event creation and update endpoints; enhance schemas for event management

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,22 +1,49 @@
 from fastapi import APIRouter
-from .schemas import EventSchema
+from .schemas import (
+    EventSchema, 
+    EventListSchema, 
+    EventCreateSchema,
+    EventUpdateSchema
+)
 
 
 router = APIRouter()
 
-
-# /api/events/
+# GET DATA HERE
+# List View
+# GET /api/events/
 @router.get("/")
-def read_events():
+def read_events() -> EventListSchema:
     # a bunch of items in a table
     return {
-        "item":[1,2,3]
+        "results":[
+            {"id": 1},{"id": 2}, {"id": 3}
+            ],
+            "count": 3
     }
 
+# SEND DATA HERE
+# create view
+# POST /api/events/
+@router.post("/")
+def create_events(payload:EventCreateSchema) -> EventSchema:
+    # a bunch of items in a table
+    print(payload.page)
+    data = payload.model_dump() # payload -> dict -> pydantic
+    return {"id": 123, **data}
 
+# GET /api/events/12
 @router.get("/{event_id}")
 def get_event(event_id: int) -> EventSchema:
     # a single row
     return {
         "id": event_id
     }
+
+# UPDATE THIS DATA
+# PUT /api/events/12
+@router.put("/{event_id}")
+def update_event(event_id: int, payload: EventUpdateSchema) -> EventSchema:
+    # a single row
+    data = payload.model_dump()
+    return {"id": event_id, **data}
